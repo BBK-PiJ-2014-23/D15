3da4690471ca32ec1acc836a1cedaafa89ac3086
unknown <smayer04@CSPC060.dcs.bbk.ac.uk>
2015-02-02 18:19:05 +0000

    First commit.

---
+++ MorePatients/MorePatients.java
@@ -0,0 +1,19 @@
+import java.util.*;
+/**
+ * Write a description of class MorePatients here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MorePatients
+{
+    public static void main(String[] args) {
+        List<Patient> list = new ArrayList<Patient>();
+        System.out.print("Enter new patient's name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.print("Enter new patient's age: ");
+        int age = sc.nextInt();
+        list.add(new Patient(name, age));
+    }
+}
